813578ee474b9c23e51924349775f0d432b60d43
Taylor Busch <Taylor.R.Busch.-ND@espn.com>
2013-09-17 19:43:21 -0400

    add charset

--- index.php
+++ index.php
@@ -1,15 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <link rel="stylesheet" type="text/css" href="style.css" />
 <title>Taylor Busch P1</title>
 </head>
 <body>
 	<div id="container">
 		<h1>Taylor Busch</h1>
-		<hr id="rule"></hr>
+		<hr id="rule" />
 		<div id="info">
-			<img id="taylor" src="taylor.jpg"></img>
+			<img id="taylor" src="taylor.jpg" alt="me" />
 			<div id="bio">Hi I'm Taylor.<br /><br />I am an Android Developer. In my free time I enjoy photography, cooking and outdoor activities.
 				<br /><br />I also really enjoy travelling. I just got back from France and Belgium a few weeks ago, and that was awesome.
 				<br /><br />I tend to just use TextPad to edit HTML and CSS manually and check it with validators later. Not much of an IDE guy when it comes to markup.
