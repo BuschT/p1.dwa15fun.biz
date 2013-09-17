111fa55ff827a7508a3cd6f9313124c32dfdb2ad
Taylor Busch <Taylor.R.Busch.-ND@espn.com>
2013-09-17 19:37:24 -0400

    initial info and styles

--- index.php
+++ index.php
@@ -5,5 +5,16 @@
 <title>Taylor Busch P1</title>
 </head>
 <body>
+	<div id="container">
+		<h1>Taylor Busch</h1>
+		<hr id="rule"></hr>
+		<div id="info">
+			<img id="taylor" src="taylor.jpg"></img>
+			<div id="bio">Hi I'm Taylor.<br /><br />I am an Android Developer. In my free time I enjoy photography, cooking and outdoor activities.
+				<br /><br />I also really enjoy travelling. I just got back from France and Belgium a few weeks ago, and that was awesome.
+				<br /><br />I tend to just use TextPad to edit HTML and CSS manually and check it with validators later. Not much of an IDE guy when it comes to markup.
+			</div>
+		</div>
+	</div>
 </body>
 </html>
